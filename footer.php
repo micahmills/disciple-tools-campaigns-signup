2c397519bb4065930c46a9b06dbd45b69c718ac0
Nath <nathinabob@gmail.com>
2022-08-25 10:29:57 +0100

    feat: add styles and structure for front page

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
 <?php wp_footer(); ?>
+
+<footer class="light-grey bg-dark-grey | footer">
+    <div class="container | footer__copywrite">
+        © Copyright
+        <script>document.write(new Date().getFullYear())</script>
+        &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        <a class="lighter-grey" href="https://disciple.tools">Disciple.Tools</a>
+        &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        All rights reserved
+        &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+       <a class="lighter-grey" href="privacy">Privacy Policy</a>
+    </div>
+</footer>
+
 </body>
 </html>
--- page.php
+++ page.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
 
+<div class="container wrapper">
+    <div class="content two-col">
+        <div>
+
+            <h1 class="section-title"><?php the_title() ?></h1>
+
+            <?php the_content() ?>
+
+        </div>
+        <div>
+            <button class="button-primary">
+                Create a campaign site
+            </button>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
