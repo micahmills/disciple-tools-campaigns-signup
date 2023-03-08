ab83a88950b502d45a745ba2b665d3e7f8988bb8
micahmills <micahmills@gmail.com>
2023-03-08 23:16:21 +0300

    remove Pray4 header image

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <header class="header">
         <div class="container">
             <div class="logo">
-                <a href="/" class="logo__link"><img class="logo__image" src="https://pray4movement.org/wp-content/uploads/2021/08/cropped-p4m-logo-medium.png" alt=""></a>
+                <a href="/" class="logo__link"><img class="logo__image" src="#" alt=""></a>
             </div>
         </div>
     </header>
